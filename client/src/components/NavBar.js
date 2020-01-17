e881e04aa09da016999e725bcf02895f8ca61d7c
nicbongo <nicbongo@protonmail.com>
2020-01-17 13:06:45 -0400

    toggle working!

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,10 +1,23 @@
 import React from "react";
+import { useTheme } from "../hooks/useTheme";
 
 const NavBar = () => {
+
+  const [darkMode, setDarkMode] = useTheme();
+
+  const toggleTheme = e => {
+    e.preventDefault();
+    setDarkMode(!darkMode)
+
+    console.log(`toggled dark mode!`)
+  }
+
+
   return (
     <div>
       <h2>Women's World Cup</h2>
-      <button onClick={""}>DarkMode</button>
+      <button onClick={toggleTheme}
+      className={darkMode ? "defaultTheme" : "altTheme"}>DarkMode</button>
     </div>
   )
 }
---
+++ client/src/hooks/useTheme.js
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+import useLocalStorage from "./useLocalStorage";
+
+export const useTheme = (baseValue) => {
+
+  const [theme, setTheme] = useLocalStorage("theme", baseValue)
+
+  useEffect(() => {
+    if(theme){
+      document.querySelector("body").classList.add("dark-mode")
+    } else {
+      document.querySelector("body").classList.remove("dark-mode")
+    }
+  }, [theme])
+
+  return [theme, setTheme]
+}
